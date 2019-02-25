c57fd9cef51dc976f717376fc74e86c4d6f0d1f8
Daniel Weinman <namniewd@gmail.com>
2019-02-25 15:53:30 -0500

    setup knex/sqlite3 environment to store users

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const helmet = require('helmet');   
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const db = require('./data/users-module.js')
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n *** Running on port ${port} **\n`);
+})
